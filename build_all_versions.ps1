c58be590a3f8d27fda6ef34b347906121dca49f7
Antonin Aubert <antonin.aubert@imta.fr>
2026-05-20 13:51:54 +0200

    feat: update project settings and add TimelineSettings asset for fishNet

--- build_all_versions.ps1
+++ build_all_versions.ps1
@@ -1,4 +1,4 @@
-$project_folders = @("base","base_DOTS","base_GPU","photonFusion","ngo")
+$project_folders = @("base","base_DOTS","base_GPU","photonFusion","ngo", "fishNet")
 $unity_path = "C:\Program Files\Unity\Hub\Editor\6000.3.7f1\Editor\Unity.exe"
 $build_path_output = Join-Path $PSScriptRoot "builds"
 
